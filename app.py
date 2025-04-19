297196775c61439f728cd5d32f468891d3a3e070
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2025-04-19 02:23:49 +0200

    Project Architecture

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return "Hello, World!  a"
+	return "Hello, World!"
+
+@app.route("/index")
+def index():
+	# 1. Open and load the JSON file
+	with open("posts.json", "r", encoding="utf-8") as fileobject:
+		blog_posts = json.load(fileobject)
+
+	# 2. Pass the list of posts into the template
+	return render_template("index.html", posts=blog_posts)
 
 
 if __name__=="__main__":
-	app.run(host="0.0.0.0", port=5000, debug=True)
+	app.run(host="0.0.0.0", port=5001, debug=True)
